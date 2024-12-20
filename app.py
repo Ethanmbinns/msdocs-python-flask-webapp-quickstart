615c5a6077c61e8a6f168159d8c440d979545996
Ethanmbinns <ethanmbinns@gmail.com>
2024-12-20 23:35:34 +0000

    modified:   app.py 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -1,33 +1,19 @@
-import os
+from flask import Flask
+from flask_socketio import SocketIO
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
-
+# Initialize Flask app
 app = Flask(__name__)
+# Initialize SocketIO with the Flask app
+socketio = SocketIO(app)
 
+# WebSocket event handler for user connection
+@socketio.on('connect')
+def handle_connect():
+    print('User connected')
+    # Emit a welcome message to the client
+    socketio.emit('message', 'Hello')
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
-
-
+# Run the app
 if __name__ == '__main__':
-   app.run()
+    # Run the SocketIO server
+    socketio.run(app, debug=True)
